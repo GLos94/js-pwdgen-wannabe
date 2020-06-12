f688c4f5d59c811168db06830f6c1fa268edc639
Giulia <giulialosito94@gmail.com>
2020-06-12 14:21:09 +0200

    Prova Bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,19 +1,23 @@
 
 // RICHIESTA 1
 // chiedere il nome e salvarlo in una var
+
 var nome = prompt("inserisci il tuo nome:");
 
 // RICHIESTA 2
 // chiedere all'utente il suo cognome
+
 var cognome = prompt("inserisci il tuo cognome");
 
 // RICHIESTA 3
 // chiedere all'utente il suo colore preferito
+
 var colorePreferito = prompt("Qual è il tuo colore preferito?");
 
 
 // RISULTATO
 // calcolo la password insicurissima
+
 var password = nome + cognome + colorePreferito;
 
 
